202b74fded188c1d5cf76fa928d730f4b98cb181
kishor <kk4988359@gmail.com>
2023-07-31 21:11:02 +0530

    first

--- todo.js
+++ todo.js
@@ -1,26 +1,23 @@
-let buttons= document.querySelectorAll('.switches button')
+let buttons = document.querySelectorAll('.switches button')
 console.log(buttons);
-buttons.forEach((button)=>{
-button.addEventListener('click',(e)=>{
-   console.dir(e.target.dataset.name,"entered button");
-   if(e.target.dataset.name=='cancel'){
-      // console.log(document.getElementById('list').value);
-      document.getElementById('list').value=" "
-   }
-   else{
-      // console.log(,"jiii");
-      if(document.getElementById('list').value.length>1){
-        let dom=document.createElement('h4') 
-         dom.textContent=document.getElementById('list').value
-      // console.log(dom.textContent);
-      dom.classList.add('attach');
-      let insert =document.getElementById("parent")
-      insert.appendChild(dom)
-      document.getElementById('list').value=" "
-      dom.addEventListener('dblclick',(e,remove)=>{
-         console.log(e.srcElement.remove())
-      })
-   }
-   }
-})
+buttons.forEach((button) => {
+   button.addEventListener('click', (e) => {
+      console.dir(e.target.dataset.name, "entered button");
+      if (e.target.dataset.name == 'cancel') {
+         document.getElementById('list').value = " "
+      }
+      else {
+         if (document.getElementById('list').value.length > 1) {
+            let dom = document.createElement('h4')
+            dom.textContent = document.getElementById('list').value
+            dom.classList.add('attach');
+            let insert = document.getElementById("parent")
+            insert.appendChild(dom)
+            document.getElementById('list').value = " "
+            dom.addEventListener('dblclick', (e, remove) => {
+               console.log(e.srcElement.remove())
+            })
+         }
+      }
+   })
 })
