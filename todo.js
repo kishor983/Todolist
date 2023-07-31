4f1285952756916cea741c8a78fbbb41dff36b88
kishor <kk4988359@gmail.com>
2023-07-31 21:47:16 +0530

    clean

--- todo.js
+++ todo.js
@@ -4,18 +4,18 @@ buttons.forEach((button) => {
    button.addEventListener('click', (e) => {
       console.dir(e.target.dataset.name, "entered button");
       if (e.target.dataset.name == 'cancel') {
-         document.getElementById('list').value = " "
+         document.getElementById('list').value = " ";
       }
       else {
          if (document.getElementById('list').value.length > 1) {
             let dom = document.createElement('h4')
-            dom.textContent = document.getElementById('list').value
+            dom.textContent = document.getElementById('list').value;
             dom.classList.add('attach');
-            let insert = document.getElementById("parent")
-            insert.appendChild(dom)
-            document.getElementById('list').value = " "
+            let insert = document.getElementById("parent");
+            insert.appendChild(dom);
+            document.getElementById('list').value = " ";
             dom.addEventListener('dblclick', (e, remove) => {
-               console.log(e.srcElement.remove())
+               console.log(e.srcElement.remove());
             })
          }
       }
